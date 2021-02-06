db778ee03ecf23334ee3f34b72990fde4f656911
chul39 <chullapatch.s@gmail.com>
2021-02-06 20:59:29 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h1>The Reaction Test</h1>
+    <button @click="startGame" :disabled="isPlaying">Start</button>
+  </div>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Result v-if="showResults" :score="score" />
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Block, 
+    Result
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    startGame() {
+      this.delay = 2000 + (Math.random() * 5000)
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,31 @@
+<template>
+  <p>Reaction time: {{ score }}ms</p>
+  <p class="comment">{{ comment }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            comment: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.comment = 'ร.... เร็ว!'
+        } else if (this.score < 500) {
+            this.comment = 'ก็ไม่เลวเลยนี่'
+        } else {
+            this.comment = 'กากจังเลยอ่ะ'
+        }
+    }
+}
+</script>
+
+<style>
+.comment {
+    font-size: 1.4em;
+    font-weight: bold;
+}
+</style>
